4882374813c72a4c45ea0091c73b61f7f4c73e7d
Clementinogm25 <MICROTECLE@GMAIL.COM>
2025-02-15 15:53:36 +0000

    Jogo Xadres_Novato

--- xadrez.c
+++ xadrez.c
@@ -3,8 +3,39 @@
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
+#define MAX_CASAS 8
 int main() {
+
+        // Movimentação do Bispo
+        printf("Movimentação do Bispo:\n");
+        int casas_bispo = 0;
+    
+        // Loop while para movimentação na diagonal superior direita
+        while (casas_bispo <=5) {
+            printf("5 casas na diagonal superior direita\n");
+            casas_bispo++;
+        }
+    
+        // Movimentação da Torre
+        printf("\nMovimentação da Torre:\n");
+        int casas_torre = 0;
+    
+        // Loop for para movimentação para a direita
+        for (int i = 0; i < 5; i++) {
+            printf("5 casas para a direita\n");
+            casas_torre++;
+        }
+    
+        // Movimentação da Rainha
+        printf("\nMovimentação da Rainha:\n");
+        int casas_rainha = 0;
+    
+        // Loop do-while para movimentação para a esquerda
+        do {
+            printf("8 casas para a esquerda\n");
+            casas_rainha++;
+        } while (casas_rainha < 8);
+    
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
